b45513d0aea67bcddaeacc1927933be592ef1efc
amsterdan <hujs@chargerlink.com>
2017-07-18 17:51:52 +0800

    del

--- lock/src/lock/method/command.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace amsterdan\lock\method;
-
-interface command
-{
-    public function set($key, $value, $time);
-    public function delete($key);
-    public function setnx($key, $value, $time);
-}
